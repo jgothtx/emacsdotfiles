ece841a0108eef13c701ac1f944a3b177fc48750
Jason Goth <jason@gothtx.com>
2023-05-05 13:29:06 -0500

    Update init.el
    use straight

--- init.el
+++ init.el
@@ -1,95 +1,79 @@
-;; Add MELPA to package list and initiatilize
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+;;
+;; Package manager  setup
+;; 
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; load my themes
-(use-package base16-theme
-  :ensure t
-  :config
-  (load-theme 'base16-ashes t))
+;; Setup use-package to use straight.el
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+(setq use-package-always-ensure t)
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
 
-;; markdown mode
-(use-package markdown-mode
-  :ensure t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+;;
+;; Language Support
+;;
 
-;; load magit
-(use-package magit
-  :ensure t
-  :bind (("C-x g" . magit-status)))
+;; Go
+(use-package go-mode)
+(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
 
-;;install and configure ivy
-(use-package ivy
-  :ensure t
-  :config
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t))
+;; Lisp
+(use-package slime)
+(setq inferior-lisp-program "sbcl")
 
-;; load swiper
-(use-package swiper
-  :ensure t
-  :config
-  (ivy-mode 1)
-  (global-set-key "\C-s" 'swiper)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume))
+;;
+;; Utilities
+;;
 
-;; set counsel
-(use-package counsel
-  :ensure t
-  :config
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-c C-f") 'counsel-find-file))
+;; Helm
+(use-package helm)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
 
+;; Version control stuff
+(use-package magit)
 
-;; setup geiser for scheme
-(use-package geiser
-  :ensure t
-  :config
-  (setq geiser-racket-binary "C:/Users/jgoth/Software/Racket/Racket.exe"))
+;; Parens
+(use-package rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-;; Load yasnippet
-(use-package yasnippet
-  :ensure t
-  :init)
+;; Autocomplete
+(use-package company)
+(add-hook 'prog-mode-hook #'company-mode)
 
-;; Javascript environment
-(use-package js2-mode
-  :ensure t
-  :init
-  :mode "\\.js\\'"
-  :config
-  (setq js-basic-indent 2)
-  (use-package js2-refactor :ensure t))
+;; 
+;; Look and feel
+;;
 
-;;(use-package xref-js2 :ensure t :init)
+;; Remove distraction
+(tool-bar-mode -1)
+;;(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t) 
+(setq ring-bell-function 'ignore)
+(defalias 'yes-or-no-p 'y-or-n-p)
 
+;; Download Color Theme Modern
+(use-package color-theme-modern)
 
-;; ~ is wierd on windows, so start in c:/Users/jgoth
-(setq default-directory "C:/Users/jgoth")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet js2-mode counsel swiper geiser slime ivy magit use-package base16-theme)))
- '(tooltip-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 100 :width normal)))))
+;; Use railscast
+(require 'color-theme-modern)
+(load-theme 'railscast t t)
+(enable-theme 'railscast)
